f15235e736aa6c70cbf056b1d6fb471a935d40c8
Grin <grinlikegreenbutgrin@gmail.com>
2020-03-30 22:34:35 +0500

    task2

--- demo-web/src/main/java/com/example/demoweb/service/PostService.java
+++ demo-web/src/main/java/com/example/demoweb/service/PostService.java
@@ -3,17 +3,24 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
+
+    private List<Post> posts;
+
+    public PostService(){
+        posts = new ArrayList<>();
+    }
+
     public List<Post> listAllPosts(){
-        return Arrays.asList(
-                new Post("hello", new Date()),
-                new Post("bonjour", new Date()),
-                new Post("arriva", new Date())
-        );
+        return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
